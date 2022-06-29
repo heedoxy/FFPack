153b4181324438e8f1364773bb40ea0c2b39c9d8
Hadi <hadboard@hotmail.com>
2022-06-29 04:37:00 -0700

    Create LoginController.php

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+
+    public function username() {
+        return 'phone';
+    }
+
+    public function login_show() {
+        return view('login');
+    }
+
+    public function login(Request $request)
+    {
+
+        $credentials = $request->only('phone', 'password');
+
+        if (Auth::attempt($credentials)) {
+            // if success login
+            return redirect('/');
+        }
+        // if failed login
+        return redirect('/login');
+    }
+}
