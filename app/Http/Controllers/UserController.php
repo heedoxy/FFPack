0dfa847ed137018df629696dfd131f8e7fd0d846
Hadi <hadboard@hotmail.com>
2022-07-01 07:03:55 -0700

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,15 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
-    public function index() {
-        return view('user-list');
+    public function index()
+    {
+        $users = User::all();
+        return view('user-list', ['users' => $users]);
     }
 
-    public function add() {
+    public function add()
+    {
         return view('user');
     }
 }
