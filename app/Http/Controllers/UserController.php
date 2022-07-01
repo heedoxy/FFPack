c6c4220d0747db774103ca2ff96a150208427ff4
Hadi <hadboard@hotmail.com>
2022-07-01 08:22:57 -0700

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+//use Illuminate\Support\Facades\Request
 
 class UserController extends Controller
 {
@@ -17,5 +18,15 @@ class UserController extends Controller
     public function add()
     {
         return view('user');
+    }
+
+    public function store(Request $request) {
+        $user = new User();
+        $user->name = $request->name;
+        $user->family = $request->family;
+        $user->password = $request->password;
+        $user->access = $request->access;
+        $user->save();
+        return redirect('/user/list')->withErrors(['success' => 'با موفقیت ثبت شد .']);
     }
 }
