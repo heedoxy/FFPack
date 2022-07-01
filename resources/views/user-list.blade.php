ded4461e11303dc8bc5ff129570f6c39b28242f7
Hadi <hadboard@hotmail.com>
2022-07-01 06:32:59 -0700

    add user views

---
+++ resources/views/user-list.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.master')
+
+@section('title', 'لیست کاربران')
+
+@section('main')
+    <main class="main-content">
+    </main>
+@endsection
---
+++ resources/views/user.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.master')
+
+@section('title', 'مدیریت کاربر')
+
+@section('main')
+    <main class="main-content">
+    </main>
+@endsection
