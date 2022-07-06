a2eda867b09ffbd02cb3d1bb8ef576b0f68c7d2a
Hadi <hadboard@hotmail.com>
2022-07-06 04:06:21 -0700

    use model variables

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    protected $table = 'products';
+    protected $primaryKey = 'id';
+    public $incrementing = true;
+    public $timestamps = true;
+    const CREATED_AT = 'created_at';
+    const UPDATED_AT = 'updated_at';
+
 }
