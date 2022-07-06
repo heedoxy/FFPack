ba4bae777bbcafd71951fc167b996ff846bfdcaf
Hadi <hadboard@hotmail.com>
2022-07-06 04:13:17 -0700

    add product main method

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,65 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function index()
+    {
+        $products = Product::all();
+        return view('product-list', ['products' => $products]);
+    }
+
+    public function add($id = null)
+    {
+        $product = null;
+        if ($id) $product = Product::find($id);
+        return view('user', ['id' => $id, 'product' => $product]);
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required',
+            'barcode' => 'required',
+            'price' => 'required',
+        ]);
+
+        $product = new Product();
+        $product->name = $request->name;
+        $product->barcode = $request->barcode;
+        $product->price = $request->price;
+        $product->description = $request->description;
+        $product->save();
+        return redirect('/product/list')->withErrors(['success' => 'با موفقیت ثبت شد .']);
+    }
+
+    public function update(Request $request)
+    {
+        $id = $request->id;
+
+        $this->validate($request, [
+            'id' => 'required',
+            'name' => 'required',
+            'barcode' => 'required',
+            'price' => 'required',
+        ]);
+
+        $product = Product::find($id);
+        $product->name = $request->name;
+        $product->barcode = $request->barcode;
+        $product->price = $request->price;
+        $product->description = $request->description;
+        $product->save();
+        return redirect('/product/list')->withErrors(['success' => 'با موفقیت ثبت شد .']);
+    }
+
+    public function remove($id) {
+        $product = Product::find($id);
+        $product->delete();
+        return redirect('/product/list')->withErrors(['danger' => 'با موفقیت حذف شد .']);
+    }
 }
