1be12c5830fc70a31e2d77983fcd76d66b5adbe9
Hadi <hadboard@hotmail.com>
2022-07-07 14:25:12 -0700

    Update FactorController.php

--- app/Http/Controllers/FactorController.php
+++ app/Http/Controllers/FactorController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Factor;
 use App\Models\FactorDetail;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class FactorController extends Controller
 {
@@ -16,8 +17,14 @@ class FactorController extends Controller
 
     public function add($id = null)
     {
-        if ($id) $details = FactorDetail::where('factor', '=', $id);
-        else $details = FactorDetail::where('status', '=', '0');
+
+        if ($id) $details = FactorDetail::where('factor', '=', $id)->get();
+        else $details = FactorDetail::where('status', '=', 0)->get();
+
+        $details = DB::table('factor_detail')
+            ->join('products', 'factor_detail.product', '=', 'products.id')
+            ->get();
+
         return view('factor', ['id' => $id, 'details' => $details]);
     }
 
@@ -31,7 +38,8 @@ class FactorController extends Controller
 
     }
 
-    public function remove($id) {
+    public function remove($id)
+    {
 
     }
 }
