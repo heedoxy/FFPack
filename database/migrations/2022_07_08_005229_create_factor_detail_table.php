1c5a96dd8bb31ef0258b683056a3b6e6e82acb1b
Hadi <hadboard@hotmail.com>
2022-07-07 13:24:08 -0700

    Create 2022_07_08_005229_create_factor_detail_table.php

---
+++ database/migrations/2022_07_08_005229_create_factor_detail_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFactorDetailTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('factor_detail', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user');
+            $table->integer('factor');
+            $table->integer('product');
+            $table->string('price', 15);
+            $table->string('comment', 500);
+            $table->smallInteger('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('factor_detail');
+    }
+}
