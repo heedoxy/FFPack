0ea54147c60a132f393f317544a4fcf6be59c71f
Hadi <hadboard@hotmail.com>
2022-07-07 10:57:50 -0700

    Update FactorController.php

--- app/Http/Controllers/FactorController.php
+++ app/Http/Controllers/FactorController.php
@@ -6,5 +6,27 @@ use Illuminate\Http\Request;
 
 class FactorController extends Controller
 {
-    //
+    public function index()
+    {
+
+    }
+
+    public function add($id = null)
+    {
+        return view('factor');
+    }
+
+    public function store(Request $request)
+    {
+
+    }
+
+    public function update(Request $request)
+    {
+
+    }
+
+    public function remove($id) {
+
+    }
 }
