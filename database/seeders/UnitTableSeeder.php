861e48a0f9ab7c7fec634bdf6eba214b4eb73bc5
Hadi <hadboard@hotmail.com>
2022-08-23 22:30:05 +0430

    add unit seeds

---
+++ database/seeders/UnitTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UnitTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('units')->insert(
+            ["title" => "کیلوگرم"],
+            ["title" => "گرم"],
+            ["title" => "عدد"],
+            ["title" => "سانتی متر"],
+            ["title" => "سانتی متر مکعب"],
+            ["title" => "پاکت"],
+            ["title" => "کارتن"]
+        );
+    }
+}
