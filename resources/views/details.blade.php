da941f3d3c0f9d2a525377f1ede772ceb8337e35
Hadi <hadboard@hotmail.com>
2022-09-11 23:07:35 +0430

    add producer access

--- resources/views/details.blade.php
+++ resources/views/details.blade.php
@@ -1,8 +1,8 @@
 @php
     $access = \Illuminate\Support\Facades\Auth::user()->access;
-   $menu_active = 1;
-   $sub_active = 8;
-   $counter = 1;
+    $menu_active = 1;
+    $sub_active = 8;
+    $counter = 1;
 @endphp
 
 @extends('layouts.master')
@@ -25,71 +25,78 @@
                 <div class="card">
                     <div class="card-body">
 
-                        <form action="/factor/status/{{ $status }}/">
-                            <div class="row">
-                                <div class="col-md-5">
-                                    <div class="input-group ">
-                                        <div class="row">
-                                            <div class="col-md-12">
-                                                <div class="form-group row">
-                                                    <label for="input-vv" class="col-form-label mr-2">نمایش
-                                                        : </label>
-                                                    <div class="col-form-label">
-                                                        <select class="form-control text-left" id="input-vv" name="s">
-                                                            <option value="0" {{ $status ? '' : 'selected' }}>
-                                                                همه
-                                                            </option>
-                                                            @foreach($statuses as $ss)
-                                                                <option
-                                                                    value="{{ $ss->id }}" {{ $ss->id == $status ? 'selected' : '' }}>
-                                                                    {{ $ss->text }}
+                        @if(in_array($access, [0, 1]))
+                            <form action="/factor/status/{{ $status }}/">
+                                <div class="row">
+                                    <div class="col-md-5">
+                                        <div class="input-group ">
+                                            <div class="row">
+                                                <div class="col-md-12">
+                                                    <div class="form-group row">
+                                                        <label for="input-vv" class="col-form-label mr-2">نمایش
+                                                            : </label>
+                                                        <div class="col-form-label">
+                                                            <select class="form-control text-left" id="input-vv"
+                                                                    name="s">
+                                                                <option value="0" {{ $status ? '' : 'selected' }}>
+                                                                    همه
                                                                 </option>
-                                                            @endforeach
-                                                        </select>
+                                                                @foreach($statuses as $ss)
+                                                                    <option
+                                                                        value="{{ $ss->id }}" {{ $ss->id == $status ? 'selected' : '' }}>
+                                                                        {{ $ss->text }}
+                                                                    </option>
+                                                                @endforeach
+                                                            </select>
+                                                        </div>
                                                     </div>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
-                                </div>
-                                <div class="col-md-5">
-                                    <div class="input-group ">
-                                        <div class="row">
-                                            <div class="col-md-12">
-                                                <div class="form-group row">
-                                                    <label for="input-ss" class="col-form-label mr-2">بازه زمانی
-                                                        : </label>
-                                                    <div class="col-form-label">
-                                                        <select class="form-control text-left" id="input-ss"
-                                                                name="days">
-                                                            <option value="10" {{ $days == 10 ? 'selected' : '' }}>بازه
-                                                                10 روزه
-                                                            </option>
-                                                            <option value="30" {{ $days == 30 ? 'selected' : '' }}>بازه
-                                                                30 روزه
-                                                            </option>
-                                                            <option value="60" {{ $days == 60 ? 'selected' : '' }}>بازه
-                                                                60 روزه
-                                                            </option>
-                                                            <option value="90" {{ $days == 90 ? 'selected' : '' }}>بازه
-                                                                90 روزه
-                                                            </option>
-                                                        </select>
+                                    <div class="col-md-5">
+                                        <div class="input-group ">
+                                            <div class="row">
+                                                <div class="col-md-12">
+                                                    <div class="form-group row">
+                                                        <label for="input-ss" class="col-form-label mr-2">بازه زمانی
+                                                            : </label>
+                                                        <div class="col-form-label">
+                                                            <select class="form-control text-left" id="input-ss"
+                                                                    name="days">
+                                                                <option value="10" {{ $days == 10 ? 'selected' : '' }}>
+                                                                    بازه
+                                                                    10 روزه
+                                                                </option>
+                                                                <option value="30" {{ $days == 30 ? 'selected' : '' }}>
+                                                                    بازه
+                                                                    30 روزه
+                                                                </option>
+                                                                <option value="60" {{ $days == 60 ? 'selected' : '' }}>
+                                                                    بازه
+                                                                    60 روزه
+                                                                </option>
+                                                                <option value="90" {{ $days == 90 ? 'selected' : '' }}>
+                                                                    بازه
+                                                                    90 روزه
+                                                                </option>
+                                                            </select>
+                                                        </div>
                                                     </div>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
-                                </div>
-                                <div class="col-md-2">
-                                    <div class="form-row justify-content-end ">
-                                        <div class="col-auto">
-                                            <button type="submit" class="btn btn-primary mb-2">نمایش</button>
+                                    <div class="col-md-2">
+                                        <div class="form-row justify-content-end ">
+                                            <div class="col-auto">
+                                                <button type="submit" class="btn btn-primary mb-2">نمایش</button>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                        </form>
+                            </form>
+                        @endif
 
                         <div class="table-responsive">
                             <table class="table">
@@ -97,11 +104,15 @@
                                 <tr>
                                     <th scope="col">#</th>
                                     <th class="text-left" scope="col">محصول</th>
-                                    <th class="text-center" scope="col">تامین کننده</th>
+                                    @if(in_array($access, [0, 1]))
+                                        <th class="text-center" scope="col">تامین کننده</th>
+                                    @endif
                                     <th class="text-left" scope="col">فاکتور</th>
                                     <th class="text-center" scope="col">قیمت (تومان)</th>
-                                    <th class="text-center" scope="col">کاربر</th>
-                                    <th class="text-center" scope="col">وضعیت</th>
+                                    @if(in_array($access, [0, 1]))
+                                        <th class="text-center" scope="col">کاربر</th>
+                                        <th class="text-center" scope="col">وضعیت</th>
+                                    @endif
                                     <th class="text-right" scope="col">مدیریت</th>
                                 </tr>
                                 </thead>
@@ -174,25 +185,75 @@
                                                 </div>
                                             @endif
 
-                                        </td>
-                                        <td class="text-center">
-                                            @if($detail->producer)
-                                            {{ $detail->prname . " " . $detail->prfamily }}
-                                            @else
-                                                - - -
+                                            @if($access  == 2)
+                                                <div class="modal fade" id="detailModal{{ $detail->id }}" tabindex="-1"
+                                                     role="dialog"
+                                                     aria-hidden="true">
+                                                    <div class="modal-dialog" role="document">
+                                                        <div class="modal-content">
+                                                            <form method="post" action="/factor/detail/producer">
+                                                                @csrf
+                                                                <input type="hidden" name="detail"
+                                                                       value="{{ $detail->id }}">
+                                                                <div class="modal-header">
+                                                                    <h5 class="modal-title text-light">
+                                                                        اعلام وضعیت
+                                                                    </h5>
+                                                                    <button type="button" class="close"
+                                                                            data-dismiss="modal"
+                                                                            aria-label="بستن">
+                                                                        <i class="ti-close"></i>
+                                                                    </button>
+                                                                </div>
+                                                                <div class="modal-body">
+                                                                    <div class="form-group  text-left">
+                                                                        <label for="comment"
+                                                                               class="col-form-label">توضیحات
+                                                                            :</label>
+                                                                        <textarea class="form-control" name="comment"
+                                                                                  id="message-text">{{ $detail->comment2 }}</textarea>
+                                                                    </div>
+                                                                </div>
+                                                                <div class="modal-footer">
+                                                                    <button type="button" class="btn btn-secondary"
+                                                                            data-dismiss="modal">بستن
+                                                                    </button>
+                                                                    <button type="submit" name="submit" value="0" class="btn btn-danger">
+                                                                        رد سفارش
+                                                                    </button>
+                                                                    <button type="submit" name="submit" value="1" class="btn btn-success">
+                                                                        تایید سفارش
+                                                                    </button>
+                                                                </div>
+                                                            </form>
+                                                        </div>
+                                                    </div>
+                                                </div>
                                             @endif
+
                                         </td>
+                                        @if(in_array($access, [0, 1]))
+                                            <td class="text-center">
+                                                @if($detail->producer)
+                                                    {{ $detail->prname . " " . $detail->prfamily }}
+                                                @else
+                                                    - - -
+                                                @endif
+                                            </td>
+                                        @endif
                                         <td class="text-left">
                                             {{ $detail->code }}
                                         </td>
                                         <td class="text-center">{{ number_format($detail->price) }}</td>
 
-                                        <td class="text-center">
-                                            {{ $detail->name . " " . $detail->family }}
-                                        </td>
-                                        <td class="text-center text-{{ $statuses[$detail->status - 1]->label }}">
-                                            {{ $statuses[$detail->status - 1]->text }}
-                                        </td>
+                                        @if(in_array($access, [0, 1]))
+                                            <td class="text-center">
+                                                {{ $detail->name . " " . $detail->family }}
+                                            </td>
+                                            <td class="text-center text-{{ $statuses[$detail->status - 1]->label }}">
+                                                {{ $statuses[$detail->status - 1]->text }}
+                                            </td>
+                                        @endif
                                         <td class="text-right">
                                             <div class="dropdown">
                                                 <a href="#" class="btn btn-light btn-floating btn-icon btn-sm"
